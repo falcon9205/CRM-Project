dfcce0016aa6b0218046cdbfe1f798d6de2e511a
Rushabh <rushabhkhandhar38@.com>
2024-06-12 16:55:34 +0530

    added the schema for all the department

---
+++ server/model/ socialMediaMarketing.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const socialMediaMarketingSchema = new mongoose.Schema({
+    id: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Employee',
+        required: true,
+    },
+    department_id: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Department',
+        required: true,
+    },
+    fullName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    post: {
+        type: String,
+        required: true,
+    },
+});
+
+const SocialMediaMarketing = mongoose.model('SocialMediaMarketing', socialMediaMarketingSchema);
+
+export default SocialMediaMarketing;
